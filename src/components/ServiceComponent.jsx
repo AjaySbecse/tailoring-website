2c7bfb2ce9fe091f3fa7f0a4cf96a6179be8e4a6
ajay <ajays.becse@gmail.com>
2025-09-02 22:17:37 +0530

    Added We also offer section

--- src/components/ServiceComponent.jsx
+++ src/components/ServiceComponent.jsx
@@ -1,6 +1,28 @@
 import "../styles/Service.css";
 import ServiceCategoriesComponent from './ServiceSectionComponent';
 
+const otherOffers = [
+    {
+        number: "01",
+        description: "To enhance comfort and give perfect structure to your outfits.",
+        title: "Lining and Padding Services"
+    },
+    {
+        number: "02",
+        description: "Quick fixes for your favorite outfits — even if they weren’t stitched by us!",
+        title: "Minor Alterations"
+    },
+    {
+        number: "03",
+        description: "Can’t decide between a boat neck or sweetheart? We’ll guide you",
+        title: "Neck and Sleeve Styling Suggestions"
+    },
+    {
+        number: "03",
+        description: "Saw a design you love? Share it — we’ll recreate it with your twist",
+        title: "Style Duplication"
+    }
+];
 
 function ServiceComponent() {
     return (
@@ -16,6 +38,22 @@ function ServiceComponent() {
                 <p className='section-title'># OUR SERVICES</p>
                 <p className='section-subtitle'> What we stitch </p>
                 <ServiceCategoriesComponent showCategories2={true} />
+            </div>
+
+            <div className="service-other-offers">
+                <p className='section-subtitle text-center'>We Also Offer</p>
+                <div className="service-other-offers-list">
+                    {otherOffers.map((offer, idx) => (
+                        <div className="service-other-offers-list-item" key={idx}>
+                            <div className="other-offer-list-number">{offer.number}</div>
+                            <div className="other-offer-list-description">
+                                <p>{offer.description}</p>
+                                <h3>{offer.title}</h3>
+                                <hr className="other-offer-list-hr" />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
