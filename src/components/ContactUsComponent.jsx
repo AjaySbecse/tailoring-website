b5d89880bda5c8de46e7f5a4ca2a89efab48a5c5
ajay <ajays.becse@gmail.com>
2025-09-09 22:00:00 +0530

    Contact us page landing section added

--- src/components/ContactUsComponent.jsx
+++ src/components/ContactUsComponent.jsx
@@ -1,8 +1,18 @@
+import '../styles/Contact.css';
 
 function ContactUsHomponent() {
     return (
         <div>
-            <h1>ContactUs Component Rendered Successfully</h1>
+            <div className="contact-us-landing">
+                <p className="landing-title">
+                    We'd Love you Hear From You
+                </p>
+                <div className="landing-description">
+                    <p className='first-line'>Whether you have a question, want to book a stitching slot, or just looking for style advice — we're here to help! <br />
+                        Reach out to us using the details below or simply drop by the shop.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
